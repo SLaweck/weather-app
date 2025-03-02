64d0aa7afbbca20985644e66cbae0df0725ff3fd
SLaweck <slaweck7@gmail.com>
2025-03-02 18:28:57 +0000

    Added accordion view

--- src/app/views/accordion-view/accordion-view.component.ts
+++ src/app/views/accordion-view/accordion-view.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { WeatherService } from '../../weather/weather.service';
+import { ForecastTableComponent } from "../../common/forecast-table/forecast-table.component";
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-accordion-view',
-  imports: [],
+  imports: [MatExpansionModule, MatIconModule, MatProgressSpinnerModule, ForecastTableComponent, DecimalPipe],
   templateUrl: './accordion-view.component.html',
   styleUrl: './accordion-view.component.scss'
 })
 export class AccordionViewComponent {
+  weatherService = inject(WeatherService);
+  citiesWeather = this.weatherService.citiesWeather;
+  cityForecat = this.weatherService.cityForecast;
+  units = this.weatherService.weatherUnits;
 
+  selectedCity = signal<string>(this.cityForecat()?.city.name || '');
+
+  onPanelOpen(cityName: string) {
+    this.weatherService.loadCityForecast(cityName);
+    this.selectedCity.set(cityName);
+  }
 }
